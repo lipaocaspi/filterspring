a1daf9fe58977cfb9bcc6e72b7ece100460ebdc7
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-08-13 09:22:55 -0500

    refactor: :card_file_box: Add INSERTs

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,5 +1,11 @@
 INSERT INTO filterspring.country (codecountry, namecountry) VALUES("COL", "Colombia");
 INSERT INTO filterspring.region (codereg, codecountry, namereg) VALUES("CUN", "COL", "Cundinamarca");
+INSERT INTO filterspring.region (codereg, codecountry, namereg) VALUES("ANT", "COL", "Antioquia");
+INSERT INTO filterspring.region (codereg, codecountry, namereg) VALUES("SAN", "COL", "Santander");
 INSERT INTO filterspring.city (codecity, codereg, namecity) VALUES("BOG", "CUN", "Bogota");
+INSERT INTO filterspring.city (codecity, codereg, namecity) VALUES("MED", "ANT", "Medellin");
+INSERT INTO filterspring.city (codecity, codereg, namecity) VALUES("BUC", "SAN", "Bucaramanga");
 
-INSERT INTO filterspring.farmacy (codecity, latfarmacy, long1, logofarmacy, namefarmacy, addressfarmacy) VALUES("BOG", 1.5, 1.5, "Pinzon", "Drogueria Pinzon", "Clle 36");
+INSERT INTO filterspring.farmacy (codecity, latfarmacy, long1, logofarmacy, namefarmacy, addressfarmacy) VALUES("BOG", 6, 6, "Rebaja", "Drogas La Rebaja", "Clle 36");
+INSERT INTO filterspring.farmacy (codecity, latfarmacy, long1, logofarmacy, namefarmacy, addressfarmacy) VALUES("MED", 4, 4, "FarmaTodo", "Farmacia FarmaTodo", "Cra 40");
+INSERT INTO filterspring.farmacy (codecity, latfarmacy, long1, logofarmacy, namefarmacy, addressfarmacy) VALUES("BUC", 5, 5, "Pinzon", "Drogueria Pinzon", "Clle 36");
